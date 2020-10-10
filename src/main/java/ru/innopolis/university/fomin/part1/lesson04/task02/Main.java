edf1eff04ebb7cf7c8eb795b1a5f3bf5665194b5
SFomin <sergei.fomin@list.ru>
2020-10-10 19:33:34 +0500

    lesson04 task02 ready! - put in a new packages - remarks correction

--- src/main/java/ru/innopolis/university/fomin/part1/lesson04/task02/Main.java
+++ src/main/java/ru/innopolis/university/fomin/part1/lesson04/task02/Main.java
@@ -1,7 +1,10 @@
-package ru.innopolis.university.fomin;
+package ru.innopolis.university.fomin.part1.lesson04.task02;
 
-import ru.innopolis.university.fomin.box.ObjectBox;
+import ru.innopolis.university.fomin.part1.lesson04.task02.box.ObjectBox;
 
+/**
+ * Class performs the task conditions lesson04.task02
+ */
 public class Main {
     public static void main(String[] args) {
         ObjectBox objectBox = new ObjectBox();
--- src/main/java/ru/innopolis/university/fomin/part1/lesson04/task02/box/ObjectBox.java
+++ src/main/java/ru/innopolis/university/fomin/part1/lesson04/task02/box/ObjectBox.java
@@ -1,22 +1,38 @@
-package ru.innopolis.university.fomin.box;
+package ru.innopolis.university.fomin.part1.lesson04.task02.box;
 
 import java.util.*;
 
+/**
+ * ObjectBox class
+ *
+ */
 public class ObjectBox {
     private List<Object> objectList;
 
+    /**
+     * Initial field class
+     */
     public ObjectBox() {
-        this.objectList = new ArrayList<Object>();
+        this.objectList = new ArrayList<>();
     }
 
+    /**
+     * @param object Object to add in collection
+     */
     public void addObject(Object object) {
         this.objectList.add(object);
     }
 
+    /**
+     * @param object Object for remove from collection
+     */
     public void removeObject(Object object) {
         this.objectList.remove(object);
     }
 
+    /**
+     * Outputting elements to the console
+     */
     public void dump() {
         for (Object object : objectList) {
             System.out.print(object + "; ");
