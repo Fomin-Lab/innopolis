21b8de0b989930ffa1ed3d5ec94d33e4ff3c6e0d
SFomin <sergei.fomin@list.ru>
2020-10-17 13:50:07 +0500

    lesson07 task01 ready!

---
+++ src/main/java/ru/innopolis/university/fomin/part1/lesson07/task01/Main.java
@@ -0,0 +1,69 @@
+package ru.innopolis.university.fomin.part1.lesson07.task01;
+
+import java.io.*;
+import java.util.Scanner;
+import java.util.Set;
+import java.util.TreeSet;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * Class performs the task conditions lesson07.task01
+ */
+public class Main {
+    public static void main(String[] args) {
+        Set<String> words = getSortedWordsInFile("lesson07-task01-input.txt");
+        writeSetToFile(words, "lesson07-task01-output.txt");
+    }
+
+    /**
+     * Search words in file and write to sorted set
+     *
+     * @param filename File name for read
+     * @return set of sorted found words
+     */
+    public static Set<String> getSortedWordsInFile(String filename) {
+        Set<String> words = new TreeSet<>();
+
+        try {
+            FileInputStream fis = new FileInputStream(filename);
+            Scanner scanner = new Scanner(fis);
+
+            Pattern pattern = Pattern.compile("[А-яA-z]+");
+            while (scanner.hasNextLine()) {
+                String line = scanner.nextLine();
+
+                Matcher matcher = pattern.matcher(line);
+                while (matcher.find()) {
+                    String s = matcher.group();
+                    words.add(s);
+                }
+            }
+
+            fis.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return words;
+    }
+
+    /**
+     * Write set of words to file
+     *
+     * @param words Set of words
+     * @param filename Output filename
+     */
+    public static void writeSetToFile(Set<String> words, String filename) {
+        try {
+            FileOutputStream fos = new FileOutputStream(filename);
+            for (String word : words) {
+                fos.write(word.getBytes());
+                fos.write('\n');
+            }
+            fos.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
