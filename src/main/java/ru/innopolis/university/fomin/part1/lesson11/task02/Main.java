8f7c7fe9e0aededb4beb0404052e5d8a8277a959
SFomin <sergei.fomin@list.ru>
2020-10-23 23:44:59 +0500

    begin task02 completion

---
+++ src/main/java/ru/innopolis/university/fomin/part1/lesson11/task02/Main.java
@@ -0,0 +1,10 @@
+package ru.innopolis.university.fomin.part1.lesson11.task02;
+
+/**
+ * Class performs the task conditions lesson11.task02
+ */
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
