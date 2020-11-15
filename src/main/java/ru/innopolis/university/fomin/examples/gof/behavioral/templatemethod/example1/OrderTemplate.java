83a62b4336f7516affdc4e9be36a7722c57a29e3
SFomin <sergei.fomin@list.ru>
2020-11-15 16:44:23 +0500

    added template method behavioral pattern examples

---
+++ src/main/java/ru/innopolis/university/fomin/examples/gof/behavioral/templatemethod/example1/OrderTemplate.java
@@ -0,0 +1,25 @@
+package ru.innopolis.university.fomin.examples.gof.behavioral.templatemethod.example1;
+
+public abstract class OrderTemplate {
+    public boolean isGift;
+
+    public abstract void doCheckout();
+    public abstract void doPayment();
+    public abstract void doReceipt();
+    public abstract void doDelivery();
+
+    public void wrapGift() {
+        System.out.println("Gift wrapped.");
+    }
+
+    public final void processOrder() {
+        doCheckout();
+        doPayment();
+        if (isGift) {
+            wrapGift();
+        } else {
+            doReceipt();
+        }
+        doDelivery();
+    }
+}
