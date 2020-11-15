7f980bd0783b257e36cb81f642eb9e56318c6a3a
SFomin <sergei.fomin@list.ru>
2020-11-15 16:35:14 +0500

    added strategy behavioral pattern examples

---
+++ src/main/java/ru/innopolis/university/fomin/examples/gof/behavioral/strategy/example1/VisaStrategy.java
@@ -0,0 +1,18 @@
+package ru.innopolis.university.fomin.examples.gof.behavioral.strategy.example1;
+
+public class VisaStrategy extends ValidationStrategy {
+    @Override
+    public boolean isValid(CreditCard creditCard) {
+        boolean isValid = creditCard.getNumber().startsWith("4");
+
+        if (isValid) {
+            isValid = creditCard.getNumber().length() == 16;
+        }
+
+        if (isValid) {
+            isValid = passesLuhn(creditCard.getNumber());
+        }
+
+        return false;
+    }
+}
