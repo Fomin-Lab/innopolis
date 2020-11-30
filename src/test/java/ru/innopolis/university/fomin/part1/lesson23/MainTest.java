8e36f829eb411191e2ed0d28926def8e6f42a6ef
SFomin <sergei.fomin@list.ru>
2020-11-30 10:24:10 +0500

    ready tests for Main class

--- src/test/java/ru/innopolis/university/fomin/part1/lesson23/MainTest.java
+++ src/test/java/ru/innopolis/university/fomin/part1/lesson23/MainTest.java
@@ -11,26 +11,62 @@ import ru.innopolis.university.fomin.part1.lesson23.dao.UserController;
 
 import java.sql.*;
 
-
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.when;
 import static org.mockito.MockitoAnnotations.initMocks;
 
+/**
+ * Test for Main class
+ */
 class MainTest {
+    /**
+     * slf4j wrapper
+     */
     private static final Logger LOGGER = LoggerFactory.getLogger("testing");
+
+    /**
+     * stub
+     */
     @Mock
     protected Connection connection;
+
+    /**
+     * stub
+     */
     @Mock
     protected ConnectionManager connectionManager;
+
+    /**
+     * stub
+     * use for all query and create @resultSet
+     */
     @Mock
     protected PreparedStatement preparedStatement;
+
+    /**
+     * stub
+     * use sql select all and create @resultSetWithFalse
+     */
     @Mock
     protected PreparedStatement preparedStatementForSelectAll;
+
+    /**
+     * stub
+     * method next return true
+     */
     @Mock
     protected ResultSet resultSet;
+
+    /**
+     * stub
+     * method next return false
+     */
     @Mock
     protected ResultSet resultSetWithFalse;
 
+    /**
+     * Execute before each test
+     */
     @BeforeEach
     void setUp() throws SQLException {
         LOGGER.trace("setUp");
@@ -42,21 +78,35 @@ class MainTest {
         when(resultSet.next()).thenReturn(true);
     }
 
+    /**
+     * Execute after each test
+     */
     @AfterEach
     void tearDown() {
         LOGGER.trace("tearDown");
     }
 
+    /**
+     * Execute before all tests
+     */
     @BeforeAll
     static void setUpAll() {
         LOGGER.trace("setUpAll");
     }
 
+    /**
+     * Execute after all tests
+     */
     @AfterAll
     static void tearDownAll() {
         LOGGER.trace("tearDownAll");
     }
 
+    /**
+     * Test on success for method demoCRUD in Main class
+     *
+     * @throws SQLException If occur sql error
+     */
     @Test
     @DisplayName("Тестируем метод demoCRUD в классе Main")
     void demoCRUDWithOk() throws SQLException {
@@ -64,13 +114,16 @@ class MainTest {
         UserController userController = new UserController(connection);
         ArticleController articleController = new ArticleController(connection);
 
+        // заглушки для roleController
         when(connection.prepareStatement(roleController.getInsertQuery(), Statement.RETURN_GENERATED_KEYS)).thenReturn(preparedStatement);
         when(connection.prepareStatement(roleController.getSelectByIdQuery())).thenReturn(preparedStatement);
         when(connection.prepareStatement(roleController.getUpdateQuery())).thenReturn(preparedStatement);
 
+        // заглушки для userController
         when(connection.prepareStatement(userController.getDeleteByIdQuery())).thenReturn(preparedStatement);
         when(connection.prepareStatement(userController.getSelectAllQuery())).thenReturn(preparedStatement);
 
+        // заглушки для getAll
         when(connection.prepareStatement(articleController.getSelectAllQuery())).thenReturn(preparedStatementForSelectAll);
         when(connection.prepareStatement(roleController.getSelectAllQuery())).thenReturn(preparedStatementForSelectAll);
         when(connection.prepareStatement(userController.getSelectAllQuery())).thenReturn(preparedStatementForSelectAll);
@@ -78,10 +131,41 @@ class MainTest {
         assertDoesNotThrow(() -> Main.demoCRUD(connection));
     }
 
+    /**
+     * Test for method demoDemo in Main class
+     * Check on NullPointerException
+     */
     @Test
     @DisplayName("Тестируем метод demoCRUD на NullPointerException в классе Main")
     void demoCRUDWithNull() {
         NullPointerException exception = assertThrows(NullPointerException.class, () -> Main.demoCRUD(null));
         assertNull(exception.getMessage());
+    }
+
+    /**
+     * Test on success for method demoSavepoint in Main class
+     *
+     * @throws SQLException If occur sql error
+     */
+    @Test
+    void demoSavepoint() throws SQLException {
+        UserController userController = new UserController(connection);
+        when(connection.prepareStatement(userController.getInsertQuery(), Statement.RETURN_GENERATED_KEYS)).thenReturn(preparedStatement);
+
+        assertDoesNotThrow(() -> Main.demoSavepoint(connection));
+    }
+
+    /**
+     * Test on success for method demoBatch in Main class
+     *
+     * @throws SQLException If occur sql error
+     */
+    @Test
+    void demoBatch() throws SQLException {
+        UserController userController = new UserController(connection);
+        when(connection.prepareStatement(userController.getInsertQuery())).thenReturn(preparedStatement);
+        when(connection.prepareStatement(userController.getUpdateQuery())).thenReturn(preparedStatement);
+
+        assertDoesNotThrow(() -> Main.demoBatch(connection));
     }
 }
